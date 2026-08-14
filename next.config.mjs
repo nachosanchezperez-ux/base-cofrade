452881ebe33f4ac00556828161536a7b6c5b615d
nachosanchezperez-ux <nacho.sanchezperez@gmail.com>
2026-08-14 19:11:56 +0200

    Externaliza Sharp en la miniatura temporal

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core'],
+  serverExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core', 'sharp'],
   images: {
     remotePatterns: [
       {
