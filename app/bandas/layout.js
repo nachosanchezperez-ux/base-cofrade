e194ed8ae928a43bb9d038bd84a0e70125daadd8
nachosanchezperez-ux <nacho.sanchezperez@gmail.com>
2026-08-19 01:44:08 +0200

    Elimina la carga tipográfica duplicada de Bandas

--- app/bandas/layout.js
+++ app/bandas/layout.js
@@ -1,8 +1,3 @@
-import '@fontsource/open-sans/400.css'
-import '@fontsource/open-sans/600.css'
-import '@fontsource/open-sans/700.css'
-import '@fontsource/open-sans/800.css'
-
 export default function BandasLayout({ children }) {
   return children
 }
