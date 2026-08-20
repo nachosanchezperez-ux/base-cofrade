6a7d2b8b1242578da4ba43a8999e38a65bb3336c
nachosanchezperez-ux <nacho.sanchezperez@gmail.com>
2026-08-21 00:55:08 +0200

    Carga los ajustes visuales de las vistas previas de pasos

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import './globals.css';
 import './typography.css';
 import './crest.css';
 import './habit-layout.css';
+import './step-preview.css';
 import HiloHeader from '@/components/HiloHeader';
 import HiloFooter from '@/components/HiloFooter';
 import JsonLd from '@/components/JsonLd';
