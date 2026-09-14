0ec9e6630676a0aff3e4e7ead0e83f395da4132f
nachosanchezperez-ux <nacho.sanchezperez@gmail.com>
2026-09-14 12:33:52 +0200

    Limpia restos de color en las fichas de bandas

--- app/bandas/[slug]/layout.js
+++ app/bandas/[slug]/layout.js
@@ -1,12 +1,13 @@
 import detailStyles from './band-detail-polish.module.css'
 import periodStyles from './band-period-emphasis.module.css'
 import headerStyles from './band-accompaniment-header-fix.module.css'
+import paletteStyles from './band-palette-cleanup.module.css'
 
 export const dynamic = 'force-dynamic'
 
 export default function BandDetailLayout({ children }) {
   return (
-    <div className={`${detailStyles.detailScope} ${periodStyles.periodScope} ${headerStyles.headerScope}`}>
+    <div className={`${detailStyles.detailScope} ${periodStyles.periodScope} ${headerStyles.headerScope} ${paletteStyles.paletteScope}`}>
       {children}
     </div>
   )
