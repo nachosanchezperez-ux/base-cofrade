9b1842910c9b3f9224ded2b49db180344226aeee
nachosanchezperez-ux <nacho.sanchezperez@gmail.com>
2026-09-17 21:54:50 +0200

    Mantiene Agenda activa al filtrar por categoría

--- components/AgendaCofradeNavFromUrl.js
+++ components/AgendaCofradeNavFromUrl.js
@@ -1,14 +1,7 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
 import AgendaCofradeNav from '@/components/AgendaCofradeNav'
 
-const NAV_CATEGORIES = new Set(['rosaries', 'devotions', 'concerts'])
-
 export default function AgendaCofradeNavFromUrl() {
-  const searchParams = useSearchParams()
-  const requestedCategory = searchParams.get('categoria') || 'all'
-  const initialCategory = NAV_CATEGORIES.has(requestedCategory) ? requestedCategory : 'all'
-
-  return <AgendaCofradeNav initialCategory={initialCategory} />
+  return <AgendaCofradeNav />
 }
